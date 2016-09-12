03deb244d6a5021fe734268ec348bc7e95241f7a
Artak87 <artak.mnatsakanyan87@gmail.com>
2016-09-12 21:20:07 +0400

    Added Group entity

---
+++ src/main/java/org/proffart/football/training/domain/Group.java
@@ -0,0 +1,50 @@
+package org.proffart.football.training.domain;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+/**
+ * Author Artak Mnatsakanyan
+ * Date 9/12/16
+ * Time 8:55 PM
+ */
+@Entity(name = "group")
+public class Group implements Serializable {
+    private static final long serialVersionUID = -785107408431887821L;
+
+    @Id
+    @GeneratedValue
+    @Column
+    private Integer groupId;
+
+    @OneToOne
+    @JoinColumn(name = "userId")
+    private User user;
+
+    @Column(length = 32)
+    private String name;
+
+    public Integer getGroupId() {
+        return groupId;
+    }
+
+    public void setGroupId(Integer groupId) {
+        this.groupId = groupId;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
