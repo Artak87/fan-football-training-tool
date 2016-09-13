e41804ea5f402da945e6d0afcdfe8fe56166c58c
Artak87 <artak.mnatsakanyan87@gmail.com>
2016-09-13 16:09:11 +0400

    Added Create Player API

---
+++ src/main/java/org/proffart/football/training/api/impl/PlayerApiImpl.java
@@ -0,0 +1,46 @@
+package org.proffart.football.training.api.impl;
+
+import org.proffart.football.training.api.PlayerApi;
+import org.springframework.stereotype.Component;
+
+import javax.ws.rs.*;
+
+/**
+ * Author Artak Mnatsakanyan
+ * Date 9/13/16
+ * Time 3:11 PM
+ */
+@Component
+@Path("/player")
+public class PlayerApiImpl implements PlayerApi {
+
+    @POST
+    @Override
+    public String createPlayer(String name) {
+        return null;
+    }
+
+    @PUT
+    @Override
+    public String savePlayer(String name) {
+        return null;
+    }
+
+    @DELETE
+    @Override
+    public String deletePlayer(String name) {
+        return null;
+    }
+
+    @GET
+    @Override
+    public String getPlayer(String name) {
+        return null;
+    }
+
+    @GET
+    @Override
+    public String getPlayers(String name) {
+        return null;
+    }
+}
