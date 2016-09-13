b756190afae1377c925e0fa1086ace606a29d259
Artak87 <artak.mnatsakanyan87@gmail.com>
2016-09-13 21:39:41 +0400

    Added Player Api interface

--- src/main/java/org/proffart/football/training/api/PlayerApi.java
+++ src/main/java/org/proffart/football/training/api/PlayerApi.java
@@ -6,9 +6,9 @@ package org.proffart.football.training.api;
  * Time 3:11 PM
  */
 public interface PlayerApi {
-    String createPlayer(String name);
+    String createPlayer(String name, String birthday, String height, String weight, String startedTrainings, String previousExperience);
     String savePlayer(Integer id);
     String deletePlayer(Integer id);
     String getPlayer(Integer id);
-    String getPlayers(Integer id);
+    String getPlayers(Integer start, Integer limit);
 }
