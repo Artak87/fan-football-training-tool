2e763b86ffff67037971100959e3acbe62719792
Artak87 <artak.mnatsakanyan87@gmail.com>
2016-09-15 17:15:28 +0400

    Added PlayerServiceImpl class

---
+++ src/main/java/org/proffart/football/training/service/PlayerServiceImpl.java
@@ -0,0 +1,32 @@
+package org.proffart.football.training.service;
+
+import org.proffart.football.training.domain.Player;
+import org.proffart.football.training.dto.PlayerDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * Author Artak Mnatsakanyan
+ * Date 9/15/16
+ * Time 3:55 PM
+ */
+@Service
+public class PlayerServiceImpl implements PleyerService {
+
+    @Override
+    public Player createPlayer(PlayerDto playerDto) {
+        return null;
+    }
+
+    @Override
+    public Player removePlayer(Integer playerId) {
+        return null;
+    }
+
+    @Override
+    public Player getPlayer(Integer playerId) {
+        return null;
+    }
+
+
+}
