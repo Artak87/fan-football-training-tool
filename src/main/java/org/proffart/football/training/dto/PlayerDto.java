d25cf5777d1e90339e5af061cf356b27971347a4
Artak87 <artak.mnatsakanyan87@gmail.com>
2016-09-15 18:40:03 +0400

    In PlayerDto class Added getters and setter for properties

--- src/main/java/org/proffart/football/training/dto/PlayerDto.java
+++ src/main/java/org/proffart/football/training/dto/PlayerDto.java
@@ -15,4 +15,60 @@ public class PlayerDto {
     private Integer weight;
     private Date startedTrainings;
     private Integer previousExperience;
+
+    public Integer getPleyerId() {
+        return pleyerId;
+    }
+
+    public void setPleyerId(Integer pleyerId) {
+        this.pleyerId = pleyerId;
+    }
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public Integer getHeight() {
+        return height;
+    }
+
+    public void setHeight(Integer height) {
+        this.height = height;
+    }
+
+    public Integer getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Integer weight) {
+        this.weight = weight;
+    }
+
+    public Date getStartedTrainings() {
+        return startedTrainings;
+    }
+
+    public void setStartedTrainings(Date startedTrainings) {
+        this.startedTrainings = startedTrainings;
+    }
+
+    public Integer getPreviousExperience() {
+        return previousExperience;
+    }
+
+    public void setPreviousExperience(Integer previousExperience) {
+        this.previousExperience = previousExperience;
+    }
 }
