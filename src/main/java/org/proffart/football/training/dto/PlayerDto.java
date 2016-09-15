99d9c647b0bad95a893f8505ab71084c758ddbc4
Artak87 <artak.mnatsakanyan87@gmail.com>
2016-09-15 15:37:15 +0400

    Create package , added PlayerDto class

---
+++ src/main/java/org/proffart/football/training/dto/PlayerDto.java
@@ -0,0 +1,18 @@
+package org.proffart.football.training.dto;
+
+import java.util.Date;
+
+/**
+ * Author Artak Mnatsakanyan
+ * Date 9/15/16
+ * Time 2:47 PM
+ */
+public class PlayerDto {
+    private Integer pleyerId;
+    private String fullname;
+    private Date birthday;
+    private Integer height;
+    private Integer weight;
+    private Date startedTrainings;
+    private Integer previousExperience;
+}
