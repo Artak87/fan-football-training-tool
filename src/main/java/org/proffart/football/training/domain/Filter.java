1b1f7032cad9b302dfd1b59a3210b7c308d73069
Artak87 <artak.mnatsakanyan87@gmail.com>
2016-09-15 15:16:58 +0400

    Added Filter class

---
+++ src/main/java/org/proffart/football/training/domain/Filter.java
@@ -0,0 +1,11 @@
+package org.proffart.football.training.domain;
+
+/**
+ * Author Artak Mnatsakanyan
+ * Date 9/15/16
+ * Time 2:55 PM
+ */
+public class Filter {
+    private Integer start;
+    private Integer limit;
+}
