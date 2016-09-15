1b83cb2aabbe01f333624f10031721c66048127a
Artak87 <artak.mnatsakanyan87@gmail.com>
2016-09-15 17:16:14 +0400

    Added PlayerService interFace

---
+++ src/main/java/org/proffart/football/training/service/PleyerService.java
@@ -0,0 +1,17 @@
+package org.proffart.football.training.service;
+
+import org.proffart.football.training.domain.Player;
+import org.proffart.football.training.dto.PlayerDto;
+
+/**
+ * Author Artak Mnatsakanyan
+ * Date 9/15/16
+ * Time 5:04 PM
+ */
+public interface PleyerService {
+    Player createPlayer(PlayerDto playerDto);
+
+    Player removePlayer(Integer playerId);
+
+    Player getPlayer(Integer playerId);
+}
