7fed09a9730e882036ee25f9c351903df676a977
Artak87 <artak.mnatsakanyan87@gmail.com>
2016-09-15 18:37:43 +0400

    In Filter class Added getters and setter for properties

--- src/main/java/org/proffart/football/training/domain/Filter.java
+++ src/main/java/org/proffart/football/training/domain/Filter.java
@@ -8,4 +8,20 @@ package org.proffart.football.training.domain;
 public class Filter {
     private Integer start;
     private Integer limit;
+
+    public Integer getStart() {
+        return start;
+    }
+
+    public void setStart(Integer start) {
+        this.start = start;
+    }
+
+    public Integer getLimit() {
+        return limit;
+    }
+
+    public void setLimit(Integer limit) {
+        this.limit = limit;
+    }
 }
